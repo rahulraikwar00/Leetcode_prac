f81383e78659b71125237a1e08141669ac647cc1
Rahul raikwar <rr200636@gmail.com>
2022-06-12 08:25:59 +0530

    Time: 9 ms (32.95%), Space: 10.9 MB (35.83%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -5,18 +5,13 @@ public:
     sort(cost.begin(), cost.end(), greater<int>());
     int len = cost.size();
     int sum = 0;
-    int to = 0;
-    for (int i = 2; i < len; i += 3)
-    {
+    for (int i = 0; i < len; i ++)
+    { if(i%3==2){
+        continue;
+    }
         sum += cost[i];
     }
-    cout << sum << endl;
-
-    for (auto i : cost)
-    {
-        to += i;
-    }
-    cout << to << endl;
-    return to - sum;
+    cout << sum<< endl;
+    return sum;
 }
 };
