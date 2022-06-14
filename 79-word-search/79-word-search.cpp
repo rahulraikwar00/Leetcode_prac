18e36de9707e8733f3fcb5474e33ecfebe83d39f
Rahul raikwar <rr200636@gmail.com>
2022-06-14 15:04:48 +0530

    Time: 331 ms (75.93%), Space: 8.1 MB (36.60%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,51 +1,30 @@
-class Solution {
-public:
-    
-    vector<int> di = {0, 1, -1, 0};
-    vector<int> dj = {1, 0, 0, -1};
-    bool findpath(vector<vector<char>> &board, string &word, int row, int col, int i, vector<vector<int>> &vis)
-    {
-        if (i == word.size())
-        {
-            // s.pop_back();
-            // cout << "found" << s << endl;
-            return true;
+ class Solution {
+    public:
+    	 bool exist(vector<vector<char> > &board, string word) {
+    		 m=board.size();
+    		 n=board[0].size();
+            for(int x=0;x<m;x++)
+                for(int y=0;y<n;y++)
+                {
+    				if(isFound(board,word.c_str(),x,y))
+    					return true;
+                }
+            return false;
         }
-        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size())
-            return false;
-        if (board[row][col] != word[i])
-            return false;
-        board[row][col] = '0';
-        for (int k = 0; k < 4; k++)
+    private:
+    	int m;
+    	int n;
+        bool isFound(vector<vector<char> > &board, const char* w, int x, int y)
         {
-            // print the path
-            // s += path[k];
-
-            if (findpath(board, word, row + di[k], col + dj[k], i + 1, vis))
+    		if(x<0||y<0||x>=m||y>=n||board[x][y]=='\0'||*w!=board[x][y])
+    			return false;
+            if(*(w+1)=='\0')
                 return true;
-            // s.pop_back();
-        }
-        board[row][col] = word[i];
-        return false;
-    }
-
-    bool exist(vector<vector<char>> &board, string word)
-    {
-
-        if (word.size() == 0)
-            return true;
-        if (board.size() == 0)
+    		char t=board[x][y];
+    		board[x][y]='\0';
+    		if(isFound(board,w+1,x-1,y)||isFound(board,w+1,x+1,y)||isFound(board,w+1,x,y-1)||isFound(board,w+1,x,y+1))
+    			return true; 
+    		board[x][y]=t;
             return false;
-        int m = board.size(), n = board[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (findpath(board, word, i, j, 0, vis))
-                    return true;
-            }
         }
-        return false;
-    }
-};
+    };
