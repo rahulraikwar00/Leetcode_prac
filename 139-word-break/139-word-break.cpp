49452c576aa1f3b04f851df89d2dec809c023094
Rahul raikwar <rr200636@gmail.com>
2022-06-17 10:27:24 +0530

    Time: 19 ms (50.30%), Space: 9.5 MB (65.42%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int dp[301];
+    bool solve(int i, string s, set<string> &wordDict)
+    {
+        if (i == s.length())
+        {
+            return true;
+        }
+        if (dp[i] != -1)
+            return dp[i];
+        string temp;
+        for (int j = i; j < s.size(); j++)
+        {
+            temp += s[j];
+            if (wordDict.find(temp) != wordDict.end())
+            {
+
+                if (solve(j + 1, s, wordDict))
+                    return dp[i] = 1;
+            }
+        }
+        return dp[i] = 0;
+    }
+
+public:
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+
+        set<string> st;
+        memset(dp, -1, sizeof(dp));
+        for (auto i : wordDict)
+            st.insert(i);
+        vector<char> dp(wordDict.size(), '0');
+        return solve(0, s, st);
+    }
+};
