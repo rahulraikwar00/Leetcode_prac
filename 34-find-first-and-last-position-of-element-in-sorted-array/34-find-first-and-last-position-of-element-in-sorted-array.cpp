00769c14589bf66d01ce1f0aed6e6c103550b6dc
Rahul raikwar <rr200636@gmail.com>
2022-07-25 11:37:58 +0530

    Time: 7 ms (90.42%), Space: 13.5 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,11 +1,66 @@
 class Solution {
 public:
-       vector<int> searchRange(vector<int> &arr, int target)
-    {
-        int lb = lower_bound(arr.begin(), arr.end(), target) - arr.begin();
-        int ub = upper_bound(arr.begin(), arr.end(), target) - arr.begin();
-        if (lb == ub)
-            return {-1, -1};
-        return {lb, ub-1};
+    int bsf(vector<int> &arr,int target){
+        int l= 0,h=arr.size()-1;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(arr[mid]<target){
+                l=mid+1;
+            }
+            else h=mid-1;
+        }
+        if(l==arr.size()||arr[l]!=target)return -1;
+        return l;
+        
+    }
+    int bsl(vector<int> &arr,int target){
+        int l= 0,h=arr.size()-1;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(arr[mid]>target){
+                h=mid-1;
+            }
+            else l=mid+1;
+        }
+        if(h<0||arr[h]!=target)return -1;
+        return h;
+    }
+    
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>ans(2);
+        ans[0] = bsf(nums, target);
+        ans[1] = bsl(nums, target);
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int bsf(vector<int>& nums, int target){
+//         int l=0, r=nums.size()-1, mid;
+//         while(l<=r){
+//             mid = (r-l)/2+l;
+//             if(nums[mid]<target)l=mid+1;
+//             else r=mid-1;
+//         }
+//         if(l==nums.size() || nums[l]!=target)return -1;
+//         return l;
+//     }
+//     int bsl(vector<int>& nums, int target){
+//         int l=0, r=nums.size()-1, mid;
+//         while(l<=r){
+//             mid = (r-l)/2+l;
+//             if(nums[mid]>target)r=mid-1;
+//             else l=mid+1;
+//         }
+//         if(r<0 || nums[r]!=target)return -1;
+//         return r;
+//     }
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         vector<int>ans(2);
+//         ans[0] = bsf(nums, target);
+//         ans[1] = bsl(nums, target);
+//         return ans;
+//     }
+// };
